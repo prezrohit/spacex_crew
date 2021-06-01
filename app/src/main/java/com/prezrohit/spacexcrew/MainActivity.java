fe3ce250f694ab0218c00ae5d666d7dbe526f25a
prezrohit <rohitsr80@gmail.com>
2021-06-01 15:58:40 +0530

    Added Dependencies and Enabled ViewBinding

--- app/src/main/java/com/prezrohit/spacexcrew/MainActivity.java
+++ app/src/main/java/com/prezrohit/spacexcrew/MainActivity.java
@@ -1,14 +1,18 @@
 package com.prezrohit.spacexcrew;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.prezrohit.spacexcrew.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+
 	}
 }
