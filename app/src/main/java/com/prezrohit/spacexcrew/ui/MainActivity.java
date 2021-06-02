7fcacbc89f45a1243d2f1147cbca579646bacafe
prezrohit <rohitsr80@gmail.com>
2021-06-02 15:56:23 +0530

    Added APK File to root directory

--- app/src/main/java/com/prezrohit/spacexcrew/ui/MainActivity.java
+++ app/src/main/java/com/prezrohit/spacexcrew/ui/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
 		}
 	}
 
-
 	private void loadFromWebService() {
 		progressDialog.show();
 		WebService webService = WebServiceClient.getRetrofit().create(WebService.class);
